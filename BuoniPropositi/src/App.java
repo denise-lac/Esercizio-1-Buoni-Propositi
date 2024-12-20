7842c2880ea4d748f35f8af644e9ebb34050a8ba
denise-lac <deniselacetera13@gmail.com>
2024-12-20 10:43:22 +0100

    Passo 2

--- BuoniPropositi/src/App.java
+++ BuoniPropositi/src/App.java
@@ -6,15 +6,46 @@ Si chiede all’utente di indicare quale dei propositi visualizzati è stato con
 viene riproposto l’elenco solo con quelli non ancora conseguiti.
 Si continua a chiedere all’utente fino a quando tutti i propositi saranno conseguiti. */
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner sc= new Scanner(System.in);
         //inizializzo i buoni propositi
-        String[] propositi={"Tenersi in forma", "Esercitarsi su Java", "Imparare qualcosa di nuovo","Viaggiare", "Praticare ingelese"};
+        ArrayList<String> propositi= new ArrayList<>();
+        propositi.add("Tenersi in forma");
+        propositi.add("Esercitarsi su Java");
+        propositi.add("Viaggiare");
+        propositi.add("Praticare l'inglese");
+        propositi.add("Imparare una nuova cosa");
 
         //stampo a video i buoni propositi utilizzando un ciclo for-each
         System.out.println("Ecco i tuoi buoni propositi: ");
         for (String p : propositi) {
-            System.out.println("    -"+p);
+            System.out.println("    - "+p);
         }
+
+        //chiedo all'utente se ha realizzato i buoni propositi
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("\nHai realizzato almeno uno dei buoni propositi elencati?   true    o   false");
+        boolean scelta= sc.nextBoolean();
+
+        //condizione dettata in base la scelta dell'utente
+        if (scelta) {
+            int i=0; //variabile che serve per numerare l'elenco
+            System.out.println("\n\nQuali dei propositi hai realizzato?");
+            System.out.println("\nDigita il numero corrispondente al buon proposito per eliminarlo dalla lista:");
+            //stampo l'elenco numerato
+            for (String p : propositi) {
+                System.out.println("    "+i+" "+p);
+                i++;
+            }
+            //l'utente sceglie cosa eliminare dalla lista
+        }
+        else {
+            System.out.println("Ti auguro di realizzare i buoni propositi elencati, riutilizza il programma quando ne avrai eseguito almeno uno.");
+        }
+        sc.close();
     }
 }
