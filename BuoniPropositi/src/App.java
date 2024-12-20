62383b3f23ca1f20464e59ad41a6dc4e018fe76b
denise-lac <deniselacetera13@gmail.com>
2024-12-20 11:38:09 +0100

    Passo 4

--- BuoniPropositi/src/App.java
+++ BuoniPropositi/src/App.java
@@ -1,11 +1,3 @@
-/*Scrivere un programma per gestire i “Buoni propositi per l'anno prossimo”.
-
-Il programma all’avvio visualizza 5 “buoni propositi” (es: “fare più attività fisica”,
-“esercitarsi su Java”, ecc.).
-Si chiede all’utente di indicare quale dei propositi visualizzati è stato conseguito, e 
-viene riproposto l’elenco solo con quelli non ancora conseguiti.
-Si continua a chiedere all’utente fino a quando tutti i propositi saranno conseguiti. */
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -35,9 +27,10 @@ public class App {
 
         if (scelta) { // stampo in caso di true
 
-            //chiedo all'utente di eliminare gli elementi che vuole dalla lista
-            boolean continua=true;  //inizializzo una variabile per far scegliere se continuare a rimuovere elementi
-            while(continua) {
+            // chiedo all'utente di eliminare gli elementi che vuole dalla lista
+            boolean continua = true; // inizializzo una variabile per far scegliere se continuare a rimuovere
+                                     // elementi
+            while (continua) {
                 int i = 0; // variabile che serve per numerare l'elenco
                 System.out.println("\nQuali dei propositi hai realizzato?");
 
@@ -48,26 +41,43 @@ public class App {
                 }
 
                 // l'utente sceglie cosa eliminare dalla lista
-                System.out.println("\nDigita il numero corrispondente al buon proposito per eliminarlo dalla lista:");
+                System.out.print("\nDigita il numero corrispondente al buon proposito per eliminarlo dalla lista:   ");
                 int eliminazione = sc.nextInt();
                 propositi.remove(eliminazione);
 
-                //chiedo all'utente se ha altri elementi da eliminare soltanto se la lista non è vuota
+                // chiedo all'utente se ha altri elementi da eliminare soltanto se la lista
+                // non è vuota
                 if (propositi.isEmpty()) {
                     System.out.println("---------------------------------------------------------------------");
                     System.out.println("\nComplimenti! Hai portato a termine tutti i tuoi buoni propositi!");
-                    continua=false;
+                    continua = false;
                 } else {
                     System.out.println("\nHai altri elementi da eliminare?    true    o   false");
-                    continua=sc.nextBoolean();
-                }                
+                    continua = sc.nextBoolean();
+                }
             }
-            System.out.println("Grazie per aver utilizzato il programma!");
+
+            //creo una condizione che varia l'ultima stampa del programma
+            if (propositi.isEmpty()) {  //se l'array è vuoto
+
+                System.out.println("Grazie per aver utilizzato il programma!\n\n");
+
+            } else {    //se ci sono ancora elementi nella lista
+
+                System.out.println("---------------------------------------------------------------------");
+                System.out.println("\nEcco la tua lista di buoni propositi aggiornata:");
+                System.out.println("\nEcco i tuoi buoni propositi: ");
+                for (String p : propositi) {
+                    System.out.println("    - " + p);
+                }
+                System.out.println(" ");
+
+            }
 
         } else { // stampo in caso di false
             System.out.println("---------------------------------------------------------------------");
             System.out.println("Ti auguro di realizzare i buoni propositi elencati!");
-            System.out.println("riutilizza il programma quando ne avrai eseguito almeno uno.");
+            System.out.println("Riutilizza il programma quando ne avrai eseguito almeno uno.\n\n");
         }
 
         sc.close();
